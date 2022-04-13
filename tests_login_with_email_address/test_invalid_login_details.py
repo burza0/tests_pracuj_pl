4ce6292584dedd21e03934bd20aed2bc3f0eeb63
Mariusz Burzyński <mariuszburzynski77@gmail.com>
2022-04-13 16:51:47 +0200

    Change of assertion method

--- tests_login_with_email_address/test_invalid_login_details.py
+++ tests_login_with_email_address/test_invalid_login_details.py
@@ -3,55 +3,35 @@ from playwright.sync_api import expect
 
 
 def test_blank_password_and_user(page):
-    # Go to https://login.pracuj.pl/
     page.goto("https://login.pracuj.pl/")
-    # Click [data-test="button-login"]
     page.locator("[data-test=\"button-login\"]").click()
-
     email = page.locator("[data-test=\"input-email\"]")
     expect(email).to_contain_text(" ")
-    assert page.inner_text(
-        "._1Y5MF > div:nth-of-type(1) ._1q5DA") == r"Wypełnij to pole.", f"Actual value: {page.inner_text('._1Y5MF > div:nth-of-type(1) ._1q5DA')}"
+    assert page.locator("text=Adres e-mailWypełnij to pole. >> [data-test=\"validation-summary-errors\"]").is_visible()
 
     password = page.locator("[data-test=\"input-password\"]")
     expect(password).to_contain_text(" ")
-    assert page.inner_text(
-        "._1Y5MF > div:nth-of-type(2) ._1q5DA") == r"Wypełnij to pole.", f"Actual value: {page.inner_text('._1Y5MF > div:nth-of-type(2) ._1q5DA')}"
+    assert page.locator(
+        "text=visibility_offHasłoWypełnij to pole. >> [data-test=\"validation-summary-errors\"]").is_visible()
 
 
 def test_unregistered_user_login(page):
-    # Go to https://login.pracuj.pl/
     page.goto("https://login.pracuj.pl/")
-    # Click [data-test="input-email"]
     page.locator("[data-test=\"input-email\"]").click()
-    # Fill [data-test="input-email"]
     page.locator("[data-test=\"input-email\"]").fill("myszkamiki1234567890@gmail.com")
-    # Click [data-test="input-password"]
     page.locator("[data-test=\"input-password\"]").click()
-    # Fill [data-test="input-password"]
     page.locator("[data-test=\"input-password\"]").fill("qwertyuiop")
-    # Click [data-test="button-login"]
     page.locator("[data-test=\"button-login\"]").click()
 
-    assert page.inner_text("._2jrjW") == r"Możliwe, że nie potwierdziłeś swojego konta lub 3 razy użyłeś złego hasła. Sprawdź pocztę lub spróbuj później."
+    locator = page.locator("[data-test=\"text-feedback-message\"]")
+    expect(locator).to_be_visible()
 
 
 def test_wrong_username(page):
-    # Go to https://login.pracuj.pl/
     page.goto("https://login.pracuj.pl/")
-
-    # Click [data-test="input-email"]
     page.locator("[data-test=\"input-email\"]").click()
-    # Fill [data-test="input-email"]
     page.locator("[data-test=\"input-email\"]").fill("dgfdgdfgdfgdf")
-    # Click [data-test="input-password"]
     page.locator("[data-test=\"input-password\"]").click()
-    # Fill [data-test="input-password"]
     page.locator("[data-test=\"input-password\"]").fill("qwertyuiop")
-    # Click [data-test="button-login"]
     page.locator("[data-test=\"button-login\"]").click()
-
-    locator = page.locator("._3HFOg > ._1q5DA")
-    expect(locator).to_be_visible()
-
 
